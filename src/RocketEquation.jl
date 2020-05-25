736d269e7ee88cdfc01b4fa92507d11c97364c95
Johann-Tobias Schäg <joto@l014.fem.tu-ilmenau.de>
2020-05-25 03:47:33 +0200

    Fix wrong number of callbacks. triagnle number 2 too high

--- src/RocketEquation.jl
+++ src/RocketEquation.jl
@@ -5,12 +5,13 @@ using DifferentialEquations
 using StaticArrays
 using LinearAlgebra
 using MultiScaleArrays
+using Logging
 
 import Gloria: onevent!, render!, update!
 using Gloria: Gloria, Window, AbstractObject, Event, Layer, Scene, iskey
 using Gloria.Shapes: Vertex, Point, circle, Polygon, intersects
 
-include("SmoothStep.jl")
+#include("SmoothStep.jl")
 
 global t_last = 0.0
 
@@ -19,10 +20,12 @@ const width, height = 1600, 900
 function attactor(du, u, p, t)
     α, β = p
     n = length(u.nodes)
+    @assert length(du.nodes)==n
     for k in 1:n
         du.nodes[k] = zero(du.nodes[k])
         for j in 1:n
             if (k == j)
+                @assert length(du.nodes[k])==4
                 du.nodes[k] .+=
                     [u.nodes[k][3], u.nodes[k][4], -β * u.nodes[k][3], -β * u.nodes[k][4]]
             else
@@ -52,13 +55,14 @@ Newton = construct(
     PhysicsLaw,
     [
         Thingy([-700.0, -350.0, 0.0, 0.0]),
-        Thingy([-550.0, -150.0, 0.0, 0.0]),#,
-        #Thingy([-600.,15.,0.,10.])#,
-        #Thingy([200.,-200.,5.,-5.])
+        Thingy([700.0, 350.0, -0.00, 0.0]),#,
+        Thingy([-600.,15.,0.,0.10])#,
+        #Thingy([200.,-200.,5.,-0.50])
     ],
 )
+#TODO: Insert index for tracking comparisions
 
-parameters = [1e-2, 0.06]
+parameters = [1e-4, 0.00009]
 
 function condition(out, u, t, integrator)
     i = 0
@@ -67,15 +71,17 @@ function condition(out, u, t, integrator)
         for l in (k+1):n
             i += 1
             out[i] = sum(abs2, u.nodes[k][1:2] .- u.nodes[l][1:2]) - 10000
+
         end
     end
+
 end
 
 function affect!(integrator, idx)
     i = 0
     u = integrator.u
     n = length(u.nodes)
-    return for k in 1:n
+    for k in 1:n
         for l in (k+1):n
             i += 1
             if idx == i
@@ -95,17 +101,20 @@ function affect!(integrator, idx)
                         (dot(v₂ - v₁, x₂ - x₁) / sum(abs2, x₂ - x₁) * (x₂ - x₁))
                     )
 
-                println("Collision handeled.")
+                #println("Collision handeled.")
 
                 m = (x₁ + x₂) / 2
 
+
+                #TODO: If two velocities are quiet similar the should be the same
+                # Shouldn't trigger again unless one is affected by external force
 
                 u.nodes[k][3:4] .= v₁
                 u.nodes[l][3:4] .= v₂
 
                 set_u!(integrator, u)
-                println(sqrt(sum(abs2, x₁ .- x₂)) - 100, ":", v₁ ./ v₂)
-                println(norm(v₁),":",norm(v₂),":",integrator.t,":",integrator.t - t_last)
+                #println(sqrt(sum(abs2, x₁ .- x₂)) - 100, ":", v₁ ./ v₂)
+                #println(norm(v₁),":",norm(v₂),":",integrator.t,":",integrator.t - t_last)
                 global t_last = integrator.t
                 break
             end
@@ -116,7 +125,8 @@ end
 cback = VectorContinuousCallback(
     condition,
     affect!,
-    (x -> Int(((x + 1) * x) / 2))(length(Newton.nodes)),
+    (x -> Int(((x - 1) * x) / 2))(length(Newton.nodes));
+    abstol=1e-5
 )
 
 
@@ -136,7 +146,7 @@ mutable struct World{Integrator} <: AbstractObject
 end
 
 const world = World(
-    init(problemp, AutoTsit5(Rosenbrock23()); save_everystep = false, callback = cback),
+    init(problemp, Tsit5(); save_everystep = false, callback = cback),
     0.0,
     0.0,
 )
@@ -153,11 +163,11 @@ function Gloria.update!(world::World, ::Gloria.AbstractLayer, t, dt)
             world.yoff = 0.0
         end
         #println(dt,"s")
-        step!(world.Integ, dt)
+        step!(world.Integ, dt*1e-8)
     end
 end
 
-const controls = Controls(0.0, 0.0, false)
+const controls = Controls(0.0, 0.0, true)
 const scene = Scene(Layer([world]), Layer([controls], show = false))
 
 
@@ -188,10 +198,15 @@ function Gloria.render!(L::Layer, obj::World, frame, fps)
     Gloria.setcolor!(window, 100, 100, 100, 255)
     Gloria.clear!(window)
     Gloria.setcolor!(window, 255, 255, 255, 255)
-    i = 2
+    i = 3
+    avg = [0, 0]
+    for x in obj.Integ.u.nodes
+        avg += x
+    end
+    avg .= avg ./ length(obj.Integ.u.nodes)
     for x in obj.Integ.u.nodes
         i += 1
-        Gloria.render!(L, circle(Vertex(0.0, 0.0), 50; samples = i), 800 + x[1], 450 + x[2])
+        Gloria.render!(L, circle(Vertex(0.0, 0.0), 50; samples = i), 800 + x[1] -avg[1], 450 + x[2] - avg[2])
     end
     return Gloria.present!(window)
 end
